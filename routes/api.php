f283fa0fcaa7472c443e5c78bfcbff3ed0b47205
Eduardo Daniel <polidoriodaniel@hotmail.com>
2022-12-30 20:48:32 -0300

    Criação das rotas api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/cliente', App\Http\Controllers\ClienteController::class);
+
+Route::apiResource('/carro', App\Http\Controllers\CarroController::class);
+
+Route::apiResource('/locacao', App\Http\Controllers\LocacaoController::class);
+
+Route::apiResource('/marca', App\Http\Controllers\MarcaController::class);
+
+Route::apiResource('/modelo', App\Http\Controllers\ModeloController::class);
