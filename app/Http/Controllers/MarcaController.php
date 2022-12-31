f6dbad4d8600aaf6903482d2ff35d29d4d3b9263
Eduardo Daniel <polidoriodaniel@hotmail.com>
2022-12-30 21:07:14 -0300

    Adicionado inserção de dados do model Marca no BD

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -35,7 +35,9 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $marca = Marca::create($request->all()); //Criar registro no banco de dados
+        
+        return $marca;
     }
 
     /**
